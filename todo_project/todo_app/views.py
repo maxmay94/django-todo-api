92b3e29eedee26501b17e4d8d88d07bd96a6e0e7
maxmay94 <maxmay94@gmail.com>
2023-05-08 19:22:17 -0400

    changes to settings

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -3,9 +3,11 @@ from .models import TodoItem
 from .serializers import TodoItemSerializer
 
 class TodoItemListCreate(generics.ListCreateAPIView):
+    print('TodoItemListCreate called')
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
 
 class TodoItemRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    print('TodoItemRetrieveUpdateDestroy called')
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "todo_app"
+    "todo_app",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,16 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:19000",
+    "http://localhost:19001",
+    "http://localhost:19002",
+]
+
 
 ROOT_URLCONF = "todo_project.urls"
 
